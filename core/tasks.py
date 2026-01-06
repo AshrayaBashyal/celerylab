c077090dc03d15764ff4c5e4ab61c2f38ea2f13d
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-06 21:44:25 +0545

    feat(celery): add send_test_email_task with retry and backoff

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
 import time
+from .services import send_test_email
 
-@shared_task
-def slow_add(x,y ):
-  time.sleep(5)
-  return x+y
+# @shared_task
+# def slow_add(x,y ):
+#   time.sleep(5)
+#   return x+y
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=10, retry_kwargs={"max_retries": 3})
+def send_test_email_task(self, to_email):
+  send_test_email(to_email)
