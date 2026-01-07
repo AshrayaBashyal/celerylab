83611ba41be33d853ab166961b46c1c1f97e6bf0
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-07 13:16:37 +0545

    chore: change indent space

--- core/views.py
+++ core/views.py
@@ -6,17 +6,17 @@ from .tasks import send_test_email_task
 
 
 class SendTestEmailView(APIView):
-    permission_classes = []
+  permission_classes = []
 
-    def post(self, request):
-        email = request.data.get("email")
+  def post(self, request):
+    email = request.data.get("email")
 
-        if not email:
-            return Response({"error": "Email required"}, status=400)
+    if not email:
+      return Response({"error": "Email required"}, status=400)
 
-        send_test_email_task.delay(email)
+    send_test_email_task.delay(email)
 
-        return Response(
-            {"message": "Email will be sent in background"},
-            status=status.HTTP_200_OK
-        )
+    return Response(
+      {"message": "Email will be sent in background"},
+      status=status.HTTP_200_OK
+    )
