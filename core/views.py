a6336054a823f197ed7111bcc7cff08f1a790dfe
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-07 13:04:40 +0545

    feat(api): add endpoint to trigger background test email via Celery

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .tasks import send_test_email_task
+
+
+class SendTestEmailView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        email = request.data.get("email")
+
+        if not email:
+            return Response({"error": "Email required"}, status=400)
+
+        send_test_email_task.delay(email)
+
+        return Response(
+            {"message": "Email will be sent in background"},
+            status=status.HTTP_200_OK
+        )
